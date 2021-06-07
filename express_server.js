41d0abc7cde559e3ffae8882bf92565d14dce35a
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-06-07 15:10:30 -0700

    created an web server in express_server.js file

---
+++ express_server.js
@@ -0,0 +1,17 @@
+
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (require, response) => {
+  response.send("Hello!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
