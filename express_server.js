aca909c084ed645c5eb089fad212ceea064e144f
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-06-08 18:25:40 -0700

    fixed button in index file, added post route to edit long URL, added some comments in server.

--- express_server.js
+++ express_server.js
@@ -1,15 +1,18 @@
-
+// require express
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+// set ejs
 app.set("view engine", "ejs");
 
+// initial data
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+// string to be used as shortURL
 const generateRandomString = () => {
   let randomString = '';
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -20,55 +23,64 @@ const generateRandomString = () => {
   return randomString;
 };
 
+// this makes request body readable
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-
+// worming up, not critical
 app.get("/", (request, response) => {
   response.send("Hello!");
 });
-
 app.get("/urls.json", (request, response) => {
   response.json(urlDatabase);
 });
-
 app.get("/hello", (request, response) => {
   response.send("<html><body>Hello <b>World</b></body> </html>\n");
 });
 
+// get request from client and render templates
 app.get("/urls", (request, response) => {
   const templateVars = { urls: urlDatabase };
   response.render("urls_index", templateVars);
 });
-
 app.get("/urls/new", (request, response) => {
   response.render("urls_new");
 });
 
+//post url and redirect to /urls/:shortURL
 app.post("/urls", (request, response) => {
   const randomString = generateRandomString();
   urlDatabase[randomString] = request.body.longURL;
   response.redirect(`/urls/${randomString}`);
 });
 
+// get request from client and render templates
 app.get("/urls/:shortURL", (request, response) => {
   const templateVars = { shortURL: request.params.shortURL, longURL: urlDatabase[request.params.shortURL]};
   response.render("urls_show", templateVars);
-  console.log();
-  
 });
 
+// get request from client and redirect to longURL: actual website
 app.get("/u/:shortURL", (request, response) => {
   const longURL = urlDatabase[request.params.shortURL];
   response.redirect(longURL);
 });
 
-app.post("/urls/:id/delete", (request, response) => {
-  const url = request.params.id;
+// post ruote to remove urls
+app.post("/urls/:shortURL/delete", (request, response) => {
+  const url = request.params.shortURL;
   delete urlDatabase[url];
   response.redirect("/urls");
 });
 
+// post route to edit longURL
+app.post("/urls/:shortURL", (request, response) => {
+  const shortURL = request.params.shortURL;
+  urlDatabase[shortURL] = request.body.longURL;
+  response.redirect(`/urls/${shortURL}`);
+});
+
+// server Listener
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
